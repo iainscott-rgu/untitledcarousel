81be9e587abcf27ab40072cde91d26f05e826cc3
Iain Scott <odyssey3007@hotmail.com>
2016-03-21 20:36:25 +0000

    go go gadget legs!!!

--- carousel.php
+++ carousel.php
@@ -4,6 +4,7 @@
     <title>Picture Slider</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="bootstrap.min.css">
     <script src="jquery.min.js"></script>
     <script src="bootstrap.min.js"></script>
